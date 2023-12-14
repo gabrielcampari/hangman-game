f2e114abc68b206878a1dc81357cedf924d1005e
Gabriel Correia <camparicorreia@gmail.com>
2023-12-14 19:43:50 -0300

    Add cabeçalho e letra inserida de forma correa(indicando a posição)

--- hangman.c
+++ hangman.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <locale.h>
 
-int main() {
+void cabecalho() { //Criando a funcao cabecalho usando void 
+printf("\n***************************************");
+printf("\n***          JOGO DA FORCA          ***");
+printf("\n***************************************\n");
 
+printf("\n");  
+}
+
+int main() { //Função principal
+setlocale(LC_ALL, "Portuguese");
+
+//VARIAVEIS
 char palavrasecreta [20]; 
 
 sprintf(palavrasecreta, "MELANCIA"); //s - de string , strigprintf, "%s" representa o sprintf o \0 atalho para 0 que serve para indicar que uma string acaba - igual o sprintf faz 
@@ -13,18 +24,31 @@ int enforcou = 0;
 char possibilidades[26];
 int tentativas = 0; 
 
+cabecalho(); //chamando a funcao, caso nao insira a funcao nao sera executada
+
 do {
 //inicio do jogo 
 
 
     for(int i=0; i < strlen(palavrasecreta); i++){ //strlen - usado para medir o tamanho de um array
+
+        int letracorreta = 0; //variavel para encontrar a letra correta e verificar se equivale
+
+
         for(int j=0; j < tentativas; j++){
+
             if(possibilidades[j] == palavrasecreta[i]){
-                printf("%c ");
+
+                letracorreta = 1; 
                 break;
             }
         }
-        printf("_ ");
+        
+        if(letracorreta) {
+            printf("%c", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
     } 
     printf("\n");
 
